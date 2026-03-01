f615e63877aafd8804f5d610045cebdcbdbdcffa
dav nguyxn <hoangson091104@gmail.com>
2026-03-01 01:17:10 -0500

    refactor: extract class merge helper

--- web/lib/utils.ts
+++ web/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+const mergeClassNames = (...inputs: ClassValue[]): string => twMerge(clsx(inputs));
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return mergeClassNames(...inputs);
 }
